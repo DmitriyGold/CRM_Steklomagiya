a9914259406f91e465f3633fed57b88df79ea55b
Дмитрий <@DESKTOP-TR2H00U>
2020-01-07 12:09:11 +0300

    CRM "Стекломагия" первая запись

---
+++ views/site/index.php
@@ -0,0 +1,51 @@
+<?php
+/* @var $this yii\web\View */
+
+$this->title = 'CRM НСК';
+
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h3>Прием и обработка заказов.</h3>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-3">
+                <h2>Заказы</h2>
+
+                <p><a class="btn btn-lg btn-success" href="#">Новый</a></p>
+                <p><a class="btn btn-lg btn-success" href="#">Текущие</a></p>
+                <p><a class="btn btn-lg btn-success" href="#">Архив</a></p>
+
+            </div>
+            <div class="col-lg-3">
+                <h2>Листовые отгрузки</h2>
+
+                <p><a class="btn btn-lg btn-success" href="#">Новый</a></p>
+                <p><a class="btn btn-lg btn-success" href="#">Текущие</a></p>
+
+
+            </div>
+            <div class="col-lg-3">
+                <h2>________</h2>
+
+                <p><a class="btn btn-lg btn-success" href="#">Номенклатура</a></p>
+                <p><a class="btn btn-lg btn-success" href="#">Статистика</a></p>                
+                <p><a class="btn btn-lg btn-success" href="#">Контрагенты</a></p>     
+                <p><a class="btn btn-lg btn-success" href="#">Сотрудники</a></p>     
+            </div>
+            <div class="col-lg-3">
+                <h2>____</h2>
+
+                <p><a class="btn btn-lg btn-success" href="#">Бухгалтерия</a></p>
+                
+                <p><a class="btn btn-lg btn-success" href="#">Неоплаченные заказы</a></p>     
+                
+            </div>
+        </div>
+
+    </div>
+</div>
